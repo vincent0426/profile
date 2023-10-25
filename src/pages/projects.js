1ace7bd40d9340d588da453f23fc9eb6b637c079
Vincent <0426vincent@gmail.com>
2023-10-26 03:54:12 +0800

    fix: typo

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,7 +3,7 @@ import { GithubIcon } from '@/components/Icon';
 import Layout from '@/components/Layout';
 import Image from 'next/image';
 import Link from 'next/link';
-import offerLand from '../../public/images/projects/offerland.png';
+import offerLand from '../../public/images/projects/offerLand.png';
 import fyno from '../../public/images/projects/fyno.png';
 import flappy from '../../public/images/projects/flappy.png';
 import covid from '../../public/images/projects/covid.png';
